ecc6ac4f47a1fba2da7336ca115783f21d7e7880
Iridius <irde@mail.ru>
2017-06-08 12:21:05 +0300

    M: Кодировка заменена на utf-8.

--- src/TestFramework/Mock.java
+++ src/TestFramework/Mock.java
@@ -11,8 +11,8 @@ import java.util.Collection;
 public class Mock {
     public static ITable getTable(){
         Collection<IColumn> columns = new ArrayList<IColumn>();
-            columns.add(new TColumnData().setKey("key1").setCaption("caption1"));
-            columns.add(new TColumnData().setKey("key2").setCaption("caption2"));
+            columns.add(new TColumnData().setKey("id").setCaption("Ключ"));
+            columns.add(new TColumnData().setKey("name").setCaption("Наименование"));
 
         ITable table = new TTableData()
                 .setName("TableData")
